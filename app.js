2e5cb42f21f74789d907f43090c0c14595811dca
Damien <connectingreflections@gmail.com>
2019-01-16 22:33:17 -0700

    pug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use (helmet.noCache());
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
-app.use('/css', express.static(path.join(__dirname, '/public/css')));
+app.use('/css', express.static(path.join(__dirname, '/public')));
 app.use('/js', express.static(path.join(__dirname, '/public/js')));
 
 app.get('/',(req, res)=> {
