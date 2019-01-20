fcc6f2dcb896d3b797d3eb225e92a2f752201638
Damien <connectingreflections@gmail.com>
2019-01-20 16:33:54 -0700

    nav bar, page layout frames

--- app.js
+++ app.js
@@ -12,18 +12,26 @@ app.use(express.static(path.join(__dirname, '/public')));
 // app.use(express.static(path.join(__dirname, '/public/js')));
 
 app.get('/',(req, res)=> {
-	res.render('home.pug', {title: 'Naturelle Therapies'});
+	res.render('home.pug', {title: 'Naturelle Therapies', path: '/' });
 });
 
 app.get('/about', (req, res) => {
-	res.render('about.pug');
+	res.render('about.pug', {path: '/about', title: 'About Me' });
 });
 
 app.get('/contact', (req, res) => {
-	res.render('contact.pug');
+	res.render('contact.pug', { path: '/contact', title: 'Contact Me' });
+});
+
+app.get('/rates', (req, res, next) => {
+	res.render('rates', { path: '/rates', title: 'Rates' });
+});
+
+app.get('/testimonials', (req, res, next) => {
+	res.render('testimonials', { path: '/testimonials', title: 'Testimonials' });
 });
 
 app.get('*', (req, res) => {
-	res.render('404', {title: "Not Found!"});
+	res.status(404) .render('404', {title: "Not Found!"});
 });
 app.listen(3000);
